d6f3f84a3131498eb9518292d2b884fb76bb084b
TomasHajek78 <tomas@tomashajek.cz>
2026-06-13 14:29:19 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'milion-plechovek-v28';
+const CACHE_NAME = 'milion-plechovek-v29';
 
 // Soubory aplikace – načítáme vždy ze sítě (network-first)
 // Důvod: při každém deployi musí testeři vidět okamžitě novou verzi
