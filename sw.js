1627c9c6f7f70eb80dc87028034340148cd94141
Tomas Hajek <haak78@Tomas-2.local>
2026-08-13 23:00:35 +0200

    Bump cache versions to force client update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'milion-plechovek-v46';
+const CACHE_NAME = 'milion-plechovek-v47';
 
 // Soubory aplikace – načítáme vždy ze sítě (network-first)
 // Důvod: při každém deployi musí testeři vidět okamžitě novou verzi
